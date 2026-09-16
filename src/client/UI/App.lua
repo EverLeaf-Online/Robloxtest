814f91f2ad2cd989f9dfeffc3d4a7afbb5052dac
Policy <173628847+iampolicy@users.noreply.github.com>
2026-09-16 12:19:01 -0400

    fix: remove invalid React root Name prop

--- src/client/UI/App.lua
+++ src/client/UI/App.lua
@@ -6,7 +6,6 @@ local React = require(ReplicatedStorage.Packages.React)
 
 local function App()
 	return React.createElement("Frame", {
-		Name = "Root",
 		BackgroundTransparency = 1,
 		BorderSizePixel = 0,
 		Size = UDim2.fromScale(1, 1),
