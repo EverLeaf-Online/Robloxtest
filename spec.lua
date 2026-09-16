7e9c67904395fd5f92b99e290c4cae71c120f728
Policy <173628847+iampolicy@users.noreply.github.com>
2026-09-16 12:08:52 -0400

    test: add Jest CLI entrypoint

---
+++ spec.lua
@@ -0,0 +1,33 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local DevPackages = ReplicatedStorage:WaitForChild("DevPackages")
+local Jest = require(DevPackages:WaitForChild("Jest"))
+local runCLI = Jest.runCLI
+
+local processServiceExists, ProcessService = pcall(function()
+	return game:GetService("ProcessService")
+end)
+
+local status, result = runCLI(ReplicatedStorage.Shared, {
+	ci = true,
+	verbose = true,
+}, { ReplicatedStorage.Shared }):awaitStatus()
+
+if status == "Rejected" then
+	warn(result)
+	if processServiceExists then
+		ProcessService:ExitAsync(1)
+	end
+	error("Jest runner rejected")
+end
+
+local success = result.results.success == true
+if processServiceExists then
+	ProcessService:ExitAsync(if success then 0 else 1)
+end
+
+if not success then
+	error("Jest tests failed")
+end
+
+return nil
