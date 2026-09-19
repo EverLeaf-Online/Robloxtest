780156bb57cb40f3edc432283f12f2f16dbe85cd
Policy <173628847+iampolicy@users.noreply.github.com>
2026-09-19 12:21:27 -0400

    Add server service integration coverage

---
+++ tests/server/Fakes/WorldService.lua
@@ -0,0 +1,13 @@
+--!strict
+
+local WorldService = {}
+
+function WorldService.GetProcessorControls(): { BasePart }
+	return {}
+end
+
+function WorldService.GetPlotAssemblers(): { BasePart }
+	return {}
+end
+
+return WorldService
