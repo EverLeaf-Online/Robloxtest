c0079c857ced210d19df580ffccc64175b8bbef0
Policy <173628847+iampolicy@users.noreply.github.com>
2026-09-19 21:38:05 -0400

    Harden salvage races and hostile collection paths

--- tests/server/Fakes/PlayerCharacter.lua
+++ tests/server/Fakes/PlayerCharacter.lua
@@ -3,13 +3,23 @@
 local PlayerCharacter = {}
 
 local nearByPlayer: { [Player]: boolean } = {}
+local positionByPlayer: { [Player]: Vector3 } = {}
 
 function PlayerCharacter.Reset()
 	table.clear(nearByPlayer)
+	table.clear(positionByPlayer)
 end
 
 function PlayerCharacter.SetNear(player: Player, near: boolean)
 	nearByPlayer[player] = near
+end
+
+function PlayerCharacter.SetPosition(player: Player, position: Vector3?)
+	positionByPlayer[player] = position
+end
+
+function PlayerCharacter.GetPosition(player: Player): Vector3?
+	return positionByPlayer[player]
 end
 
 function PlayerCharacter.IsNear(player: Player, _part: BasePart, _maxDistance: number): boolean
