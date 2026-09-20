8cc9da86b40da9d5c1c649143a96b76594c638bd
Policy <173628847+iampolicy@users.noreply.github.com>
2026-09-19 22:53:57 -0400

    Add production service integration coverage
    * Add production service integration coverage
    
    * Add production service integration coverage
    
    * Add production service integration coverage
    
    * Add production service integration coverage

--- tests/server/Fakes/StateService.lua
+++ tests/server/Fakes/StateService.lua
@@ -4,10 +4,12 @@ local StateService = {}
 
 local lastResultByPlayer: { [Player]: any } = {}
 local snapshotPushCountByPlayer: { [Player]: number } = {}
+local productionDeltaCountByPlayer: { [Player]: number } = {}
 
 function StateService.Reset()
 	table.clear(lastResultByPlayer)
 	table.clear(snapshotPushCountByPlayer)
+	table.clear(productionDeltaCountByPlayer)
 end
 
 function StateService.ActionResult(
@@ -29,12 +31,20 @@ function StateService.PushSnapshot(player: Player)
 	snapshotPushCountByPlayer[player] = (snapshotPushCountByPlayer[player] or 0) + 1
 end
 
+function StateService.PushProductionDelta(player: Player)
+	productionDeltaCountByPlayer[player] = (productionDeltaCountByPlayer[player] or 0) + 1
+end
+
 function StateService.GetLastResult(player: Player): any?
 	return lastResultByPlayer[player]
 end
 
 function StateService.GetSnapshotPushCount(player: Player): number
 	return snapshotPushCountByPlayer[player] or 0
+end
+
+function StateService.GetProductionDeltaCount(player: Player): number
+	return productionDeltaCountByPlayer[player] or 0
 end
 
 return StateService
